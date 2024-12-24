c42025c4736fdeff840c62e5d40410b02ead1376
fazlehaq <fazlehaqs360@gmail.com>
2024-12-24 17:26:51 +0530

    Added function declaration and global variables

--- v2/main.c
+++ v2/main.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Macro Definations
+#define DB_DIR_PATH "./db"
+#define DUMP_FILE_PATH "./io/tmp"
+
+
+// Function declrations
+int set_file(char *);
+int set_db(char *);
+int set_lang_flag(char *); // either -d (devnagiri) or -e (unicode english)
+int read_into_the_file(int,int); 
+
+// Global Variables
+char *DB_PATH;
+char *FILE_NAME;
+char *LANG_FLAG;
 
 int main(){
     return 0;
