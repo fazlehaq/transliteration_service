8b71e7ae731b8a825410f06def08e6b73092fee8
fazlehaq <fazlehaqs360@gmail.com>
2025-01-08 13:56:12 +0530

    added helper functions

--- v2/headers/db.h
+++ v2/headers/db.h
@@ -1,6 +1,16 @@
-#define FILE_SCHEMA \
-    "BEGIN TRANSACTION;\n" \
-    "CREATE TABLE `FILE_CONTENT` (`line_no` INTEGER PRIMARY KEY, `CONTENT` TEXT);\n" \
-    "DELETE FROM sqlite_sequence;\n" \
-    "COMMIT;\n"
+#ifndef DB_H
+#define DB_H
+#include<sqlite3.h>
 
+#define FILE_SCHEMA "BEGIN TRANSACTION;\
+CREATE TABLE `FILE_CONTENT` (`line_no` INTEGER PRIMARY KEY, `TEXT` TEXT);\
+CREATE TABLE `LINE_MAPPING` (`virtual_start` INTEGER NOT NULL , `virtual_end` INTEGER NOT NULL, `actual_start` INTEGER NOT NULL , `actual_end` INTEGER NOT NULL);\
+COMMIT;"
+
+sqlite3 * init_empty_db(char *file_name);
+sqlite3* open_database(const char *db_name);
+void close_database(sqlite3 *db);
+int execute_query(sqlite3 *db, const char *sql);
+
+
+#endif
---
+++ v2/src/db.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<sqlite3.h>
+#include"../headers/db.h"
+#include"../headers/utils.h"
+
+sqlite3* open_database(const char *db_name) {
+    sqlite3 *db;
+    int rc = sqlite3_open(db_name, &db);
+    if (rc) {
+        fprintf(stderr, "Error opening database (%s): %s\n", db_name, sqlite3_errmsg(db));
+        return NULL;
+    }
+    return db;
+}
+
+void close_database(sqlite3 *db) {
+    if (db) {
+        sqlite3_close(db);
+    }
+}
+
+    int execute_query(sqlite3 *db, const char *sql) {
+    char *errmsg = NULL;
+    int rc = sqlite3_exec(db, sql, 0, 0, &errmsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", errmsg);
+        sqlite3_free(errmsg);
+        return 0; // Failure
+    }
+    return 1; // Success
+}
+
+sqlite3 * init_empty_db(char *file_name){
+    FILE * file = create_new_file(file_name);
+    if(file == NULL) 
+        return NULL;
+    fclose(file);
+
+    sqlite3 *db = open_database(file_name);
+    if(db == NULL) 
+        return NULL;
+
+    char *sql = FILE_SCHEMA;
+    
+    if(execute_query(db,sql)) {
+        printf("Initialized sucessfully\n");
+        return db;
+    }
+
+    // Base case
+    printf("Could not create file\n");
+    return NULL;
+}
